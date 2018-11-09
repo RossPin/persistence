5ecbf2823d92c596f34657130a7140ca100016e7
RossPin <dorpetz@hotmail.com>
2018-11-09 17:14:03 +1300

    makeNomination function

--- server/autoplay.js
+++ server/autoplay.js
@@ -1,19 +1,49 @@
 const {currentGames} = require('./currentGames')
+const db = require('../db/game')
+const {checkVotes, checkIntentions, checkNominations} = require('../gameFunctions')
 
-function getAutoPlayers(game_id) {
-  return currentGames[gameId].players.filter(x => x.id <= 10)
+function getAutoPlayers(game) {
+  return game.players.filter(player => player.id <= 10)
 }
 
-function checkAutoPlayers(game_id) {
-  getAutoPlayers(game_id).map(player => {
-    if (nominateRequired(game_id, player)) makeNomination(game_id, player)  
+function checkAutoPlayers(game_id, callback) {
+  const game = currentGames[game_id]
+  getAutoPlayers(game).map(autoPlayer => {
+    if (nominateRequired(game, autoPlayer)) makeNomination(game, autoPlayer, callback)  
   })
 }
 
-function nominateRequired(game_id, player) {
-  return (currentGames[gameId].currentRound.leader_id === player.id && currentGames[gameId].gameStage === 'nominating')
+function nominateRequired(game, autoPlayer) {
+  return (game.currentRound.leader_id === autoPlayer.id && game.gameStage === 'nominating')
 }
 
-function makeNomination(game_id, player) {
-  
+function makeNomination(game, autoPlayer, callback) {
+  setTimeout(() => {
+    const {id: round_id, round_num} = game.currentRound 
+    const {mission_num} = game.currentMission
+    const numNominations = game.missionParams[mission_num - 1].team_total
+    const nominations = game.missions[mission_num-1].rounds[round_num-1].nominations
+    let nominee = isNominated(autoPlayer, nominations) ? selectNominee(game, nominations) : autoPlayer.id
+    db.castNomination(round_id, nominee).then(() => {
+      db.getNominations(round_id).then(updatedNominations => {
+        console.log('nomination recieved')
+          game.missions[mission_num-1].rounds[round_num-1].nominations = updatedNominations
+          callback(game)
+          if (updatedNominations.length <= numNominations) makeNomination(game, autoPlayer, callback)
+          else checkNominations(game_id, round_id).then(() => {
+            callback(game)
+          })
+      })
+    })
+  },2000) 
 }
+
+function isNominated(player, nominations) {
+  return nominations.filter(nomination => nomination.user_id === player.id).length > 0
+}
+
+function selectNominee(game, nominations) {
+  const unNominated = game.players.filter(player => !isNominated(player, nominations))
+  return (unNominated[Math.floor(Math.random()*unNominated.length)].id)
+}
+
