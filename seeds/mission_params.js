12f9feb810741722de909e41f20ef75dff444fca
RossPin <dorpetz@hotmail.com>
2018-11-09 14:46:18 +1300

    correct fails needed in 8 player

--- seeds/mission_params.js
+++ seeds/mission_params.js
@@ -26,7 +26,7 @@ exports.seed = function(knex, Promise) {
         {players_total: 8, team_total: 3 , mission_num:1 , fails_needed: 1 },
         {players_total: 8, team_total: 4 , mission_num:2 , fails_needed: 1 },
         {players_total: 8, team_total: 4 , mission_num:3, fails_needed: 1 },
-        {players_total: 8, team_total: 5 , mission_num:4 , fails_needed: 4 },
+        {players_total: 8, team_total: 5 , mission_num:4 , fails_needed: 2 },
         {players_total: 8, team_total: 5 , mission_num:5 , fails_needed: 1 },
         // round for nine players
         {players_total: 9, team_total: 3 , mission_num:1 , fails_needed: 1 },
