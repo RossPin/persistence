67642f2aa30577b0206d5f54d530b6a3dea5d392
RossPin <dorpetz@hotmail.com>
2018-11-13 13:20:46 +1300

    add guest login

--- seeds/users.js
+++ seeds/users.js
@@ -9,12 +9,12 @@ exports.seed = function(knex, Promise) {
         {id: 2, user_name: 'auto2', 'display_name': 'Lisa', img: '/lisa.jpg', hash: 'xxx'},
         {id: 3, user_name: 'auto3', 'display_name': 'Morty', img: '/Morty.png', hash: 'xxx'},
         {id: 4, user_name: 'auto4', 'display_name': 'Jasmine', img: '/Jasmine.jpeg', hash: 'xxx'},
-        {id: 5, user_name: 'auto5', 'display_name': 'Bender', img: '/Bender.jpg', hash: 'xxx'},
+        {id: 5, user_name: 'auto5', 'display_name': 'Bender', img: '/Bender.png', hash: 'xxx'},
         {id: 6, user_name: 'auto6', 'display_name': 'Meg', img: '/Meg.jpg', hash: 'xxx'},
         {id: 7, user_name: 'auto7', 'display_name': 'Kenny', img: '/Kenny.png', hash: 'xxx'},
         {id: 8, user_name: 'auto8', 'display_name': 'Summer', img: '/Summer.png', hash: 'xxx'},
         {id: 9, user_name: 'auto9', 'display_name': 'Cartman', img: '/Cartman.png', hash: 'xxx'},
-        {id: 10, user_name: 'guest', 'display_name': 'Guest', img: '/Guest.png', hash: 'xxx'}        
+        {id: 10, user_name: 'guest', 'display_name': 'Guest', img: '/Guest.png', hash: '$2a$12$R1vHDuSaO1GhpT4CYZrfi.cXE2qKkvYWGUvdGq0PZUJEHpX1mk7RK'}        
       ]);
     });
 };
