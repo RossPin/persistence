dac229d20168f28faff6bad3509f3310cc4af61a
RossPin <dorpetz@hotmail.com>
2018-11-13 13:20:33 +1300

    add button to login as guest

--- client/components/Auth/Login.jsx
+++ client/components/Auth/Login.jsx
@@ -11,6 +11,7 @@ class Login extends React.Component {
     }
     this.updateDetails = this.updateDetails.bind(this)
     this.submit = this.submit.bind(this)
+    this.guestLogin = this.guestLogin.bind(this)
   }
   updateDetails(e) {
     this.setState({[e.target.name]: e.target.value})
@@ -19,7 +20,10 @@ class Login extends React.Component {
     e.preventDefault()
     let {user_name, password} = this.state
     this.props.dispatch(loginUser({user_name, password}))
+  }
 
+  guestLogin() {
+    this.props.dispatch(loginUser({user_name: 'guest', password: 'GuestLoginPassword'}))
   }
   render() {
     return (
@@ -30,7 +34,8 @@ class Login extends React.Component {
         <label className="has-text-white is-size-4">Password:
           <input style={{margin: '0.5vw'}} className="input is-medium is-rounded" type="password" name="password" onChange={this.updateDetails}/>
         </label><br/>
-        <input className="button is-white is-outlined is-medium" type="submit" />
+        <input className="button is-white is-outlined is-medium" type="submit" value="Login" />
+        <span className="button is-white is-outlined is-medium" onClick={this.guestLogin}>Continue as Guest</span>
       </form>
     )
   }
